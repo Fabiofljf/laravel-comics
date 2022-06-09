53059a7b6e1e95485995655b0758c996ee6b3e73
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-09 15:51:19 +0200

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,44 +15,13 @@
     <body>
         sono in app.php
 
-        <header id="site_header">
-            <div class="UpBarr">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <ul class="text-uppercase">
-                                <li>dc power™visa®</li>
-                                <li>additional dc sites <span>&#8964;</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.UpBarr -->
-
-            <div class="DownBarr">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            logo
-                        </div>
-                        <div class="col">
-                            navbarr
-                        </div>
-                        <div class="col">
-                            serch
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.navBarr -->
-        </header>
+        @include('partials.header')
         <!-- /#site_header -->
 
         <main id="site_main"></main>
         <!-- /#site_main -->
 
-        <footer id="site_footer"></footer>
+        @include('partials.footer')
         <!-- /#site_footer -->
 
         <!-- script js -->
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<header id="site_header">
+            <div class="UpBarr">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <ul class="text-uppercase">
+                                <li>dc power™visa®</li>
+                                <li>additional dc sites <span>&#8964;</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.UpBarr -->
+
+            <div class="DownBarr">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            logo
+                        </div>
+                        <div class="col">
+                            navbarr
+                        </div>
+                        <div class="col">
+                            serch
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.navBarr -->
+        </header>
